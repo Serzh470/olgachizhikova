fd29dfd12f3fae1f10432ad84d9029e90377253a
Sergey Matyushkov <smatyushkov@arenamb.ru>
2020-02-12 17:39:47 +0300

    add SEO translation

--- src/components/common/Layout/Layout.js
+++ src/components/common/Layout/Layout.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { ThemeProvider } from 'styled-components';
+import React from "react";
+import PropTypes from "prop-types";
+import { ThemeProvider } from "styled-components";
+import { useIntl } from "gatsby-plugin-intl"
 
-import SEO from '@common/SEO';
 
-import theme from '@styles/theme';
-import GlobalStyles from '@styles/GlobalStyles';
+import SEO from "@common/SEO";
 
-const Layout = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <>
-      <SEO />
-      <GlobalStyles />
-      {children}
-    </>
-  </ThemeProvider>
-);
+import theme from "@styles/theme";
+import GlobalStyles from "@styles/GlobalStyles";
+
+const Layout = ({ children }) => {
+  const intl = useIntl();
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <SEO
+          lang={intl.locale}
+          title={intl.formatMessage({ id:"title" })}
+          keywords={intl.formatMessage({ id:"keywords" })}
+          description={intl.formatMessage({ id:"description" })}
+        />
+        <GlobalStyles />
+        {children}
+      </>
+    </ThemeProvider>
+  );
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
